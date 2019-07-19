1cfec6b1bd73bf548dec530513507923b4eb014a
ArtemPopof <Deviling42@gmail.com>
2019-07-20 00:02:10 +0300

    add part button added with stub

--- app/src/main/java/ru/abbysoft/wisebuild/MainActivity.java
+++ app/src/main/java/ru/abbysoft/wisebuild/MainActivity.java
@@ -3,12 +3,31 @@ package ru.abbysoft.wisebuild;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+/**
+ * MainActivity class
+ *
+ * Here you can choose action to do
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when addPartButton clicked
+     * @param view addPartButton
+     */
+    public void addPartButtonClicked(View view) {
+        // TODO show activity with part creation
+        // read about activities and fragments
+        // decide what to use it that case
+
+        Toast.makeText(this, "This action is not done yet", Toast.LENGTH_LONG).show();
     }
 }
