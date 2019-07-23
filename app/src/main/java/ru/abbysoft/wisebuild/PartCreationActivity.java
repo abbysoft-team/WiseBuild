a32987f02b04f2e328ac90a71de3291541cc1e8b
ArtemPopof <Deviling42@gmail.com>
2019-07-23 12:37:21 +0300

    finished layout for part creation activity

--- app/src/main/java/ru/abbysoft/wisebuild/PartCreationActivity.java
+++ app/src/main/java/ru/abbysoft/wisebuild/PartCreationActivity.java
@@ -8,16 +8,26 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import ru.abbysoft.wisebuild.model.ComputerPart;
+import ru.abbysoft.wisebuild.model.MemoryModule;
+import ru.abbysoft.wisebuild.model.Motherboard;
 
 /**
  * Specify parameters for new component
@@ -32,6 +42,12 @@ public class PartCreationActivity extends AppCompatActivity {
 
     private volatile ComputerPart.ComputerPartType partType;
     private ImageView imageView;
+    private TextView additionalParamLabel1;
+    private TextView additionalParamLabel2;
+    private TextView additionalParamSpinnerLabel;
+    private EditText additionalParamField1;
+    private EditText additionalParamField2;
+    private Spinner additionalParamSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +55,15 @@ public class PartCreationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_part_creation);
 
         imageView = findViewById(R.id.new_component_image);
+        additionalParamLabel1 = findViewById(R.id.additional_parameter_1_label);
+        additionalParamLabel2 = findViewById(R.id.additional_parameter_2_label);
+        additionalParamField1 = findViewById(R.id.additional_parameter_1_field);
+        additionalParamField2 = findViewById(R.id.additional_parameter_2_field);
+        additionalParamSpinnerLabel = findViewById(R.id.additional_param_spinner_label);
+        additionalParamSpinner = findViewById(R.id.additional_param_spinner);
 
         getPassedExtras();
+        addAdditionalFields();
     }
 
     private void getPassedExtras() {
@@ -54,10 +77,79 @@ public class PartCreationActivity extends AppCompatActivity {
         headerMessage.setText(getString(R.string.creating_part_message) + partType.getReadableName());
     }
 
+    private void addAdditionalFields() {
+        switch (partType) {
+            case CPU:
+                addFieldsForCPU();
+                break;
+            case MEMORY_MODULE:
+                addFieldsForMemory();
+                break;
+            case MOTHERBOARD:
+                addFieldsForMotherboard();
+                break;
+
+                default:
+                    break;
+        }
+    }
+
+    private void addFieldsForCPU() {
+        ((ViewManager)additionalParamSpinnerLabel.getParent())
+                .removeView(additionalParamSpinnerLabel);
+        ((ViewManager)additionalParamSpinner.getParent())
+                .removeView(additionalParamSpinner);
+
+        additionalParamLabel1.setText(getString(R.string.manufacturer));
+        additionalParamLabel2.setText(getString(R.string.num_of_cores));
+
+        additionalParamField2.setInputType(InputType.TYPE_CLASS_NUMBER);
+    }
+
+    private void addFieldsForMemory() {
+        ((ViewManager)additionalParamField2.getParent()).removeView(additionalParamField2);
+        ((ViewManager)additionalParamLabel2.getParent()).removeView(additionalParamLabel2);
+
+        additionalParamLabel1.setText(getString(R.string.num_of_cores));
+        additionalParamField1.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        additionalParamSpinnerLabel.setText(getString(R.string.type));
+
+        configureSpinnerForMemory();
+    }
+
+    private void configureSpinnerForMemory() {
+        MemoryModule.MemoryType[] array = MemoryModule.MemoryType.values();
+        ArrayAdapter<MemoryModule.MemoryType> adapter =
+                new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, array);
+        additionalParamSpinner.setAdapter(adapter);
+    }
+
+    private void addFieldsForMotherboard() {
+        ((ViewManager)additionalParamField1.getParent()).removeView(additionalParamField1);
+        ((ViewManager)additionalParamField2.getParent()).removeView(additionalParamField2);
+        ((ViewManager)additionalParamLabel1.getParent()).removeView(additionalParamLabel1);
+        ((ViewManager)additionalParamLabel2.getParent()).removeView(additionalParamLabel2);
+
+        additionalParamSpinnerLabel.setText(getString(R.string.socket));
+
+        configureSpinnerForMotherboard();
+    }
+
+    private void configureSpinnerForMotherboard() {
+        Motherboard.SocketType[] array = Motherboard.SocketType.values();
+        ArrayAdapter<Motherboard.SocketType> adapter =
+                new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, array);
+        additionalParamSpinner.setAdapter(adapter);
+    }
+
+
     /**
      * Save component with current parameters
+     *
+     * @param view button
      */
-    public void savePart() {
+    public void savePart(View view) {
 
     }
 
