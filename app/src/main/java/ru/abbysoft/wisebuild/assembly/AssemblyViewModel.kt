4e81edfe523e472f414054d32a6be4471ae69755
ArtemPopof <Deviling42@gmail.com>
2019-08-05 16:28:52 +0300

    realise add part action

--- app/src/main/java/ru/abbysoft/wisebuild/assembly/AssemblyViewModel.kt
+++ app/src/main/java/ru/abbysoft/wisebuild/assembly/AssemblyViewModel.kt
@@ -1,5 +1,6 @@
 package ru.abbysoft.wisebuild.assembly
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.abbysoft.wisebuild.model.CPU
 import ru.abbysoft.wisebuild.model.ComputerPart
@@ -13,22 +14,56 @@ import ru.abbysoft.wisebuild.model.Motherboard
  */
 class AssemblyViewModel : ViewModel() {
 
+    val nextAction : MutableLiveData<AssemblyAction> = MutableLiveData()
+
     val cpuUnits : ArrayList<CPU> = ArrayList()
     val memoryModules : ArrayList<MemoryModule> = ArrayList()
-    val motherboards : ArrayList<MemoryModule> = ArrayList()
+    val motherboards : ArrayList<Motherboard> = ArrayList()
 
     var currentCpu : CPU? = null
     var currentMotherboard : Motherboard? = null
     var currentMemory : MemoryModule? = null
 
-    fun addPart(type : ComputerPart.ComputerPartType) {
-
+    fun addButtonClicked(type : ComputerPart.ComputerPartType) {
+        nextAction.value = when (type) {
+            ComputerPart.ComputerPartType.CPU -> AssemblyAction.ADD_CPU
+            ComputerPart.ComputerPartType.MEMORY_MODULE -> AssemblyAction.ADD_MEMORY
+            ComputerPart.ComputerPartType.MOTHERBOARD -> AssemblyAction.ADD_MOTHERBOARD
+            ComputerPart.ComputerPartType.ASSEMBLED_PC -> {AssemblyAction.NOTHING}
+        }
     }
 
-    fun partCount(type : ComputerPart.ComputerPartType) : Int = when(type) {
-        ComputerPart.ComputerPartType.CPU -> cpuUnits.size
-        ComputerPart.ComputerPartType.MEMORY_MODULE -> memoryModules.size
-        ComputerPart.ComputerPartType.MOTHERBOARD -> motherboards.size
-        ComputerPart.ComputerPartType.ASSEMBLED_PC -> -1 // unwanted branch
+    fun saveAssemblyClicked() {
+        nextAction.value = AssemblyAction.SAVE_ASSEMBLY
+    }
+
+    fun actionDone() {
+        nextAction.value = AssemblyAction.NOTHING
+    }
+
+    fun newPartAdded(part: ComputerPart, partType: ComputerPart.ComputerPartType) {
+        when (partType) {
+            ComputerPart.ComputerPartType.CPU -> {
+                currentCpu = part as CPU
+                cpuUnits.add(currentCpu as CPU)
+            }
+            ComputerPart.ComputerPartType.MEMORY_MODULE -> {
+                currentMemory = part as MemoryModule
+                memoryModules.add(part)
+            }
+            ComputerPart.ComputerPartType.MOTHERBOARD -> {
+                currentMotherboard = part as Motherboard
+                motherboards.add(currentMotherboard as Motherboard)
+            }
+            ComputerPart.ComputerPartType.ASSEMBLED_PC -> {}
+        }
+    }
+
+    enum class AssemblyAction {
+        NOTHING,
+        ADD_CPU,
+        ADD_MOTHERBOARD,
+        ADD_MEMORY,
+        SAVE_ASSEMBLY
     }
 }
--- app/src/main/java/ru/abbysoft/wisebuild/assembly/SelectedPartsFragment.kt
+++ app/src/main/java/ru/abbysoft/wisebuild/assembly/SelectedPartsFragment.kt
@@ -1,32 +1,74 @@
 package ru.abbysoft.wisebuild.assembly
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import ru.abbysoft.wisebuild.R
+import ru.abbysoft.wisebuild.browser.BrowserActivity
 import ru.abbysoft.wisebuild.databinding.FragmentPartOverviewBinding
 import ru.abbysoft.wisebuild.model.ComputerPart
+import ru.abbysoft.wisebuild.storage.DBFactory
+import ru.abbysoft.wisebuild.utils.ModelUtils
+import java.lang.IllegalStateException
 
 
 /**
  * Fragment with list of selected parts with specific type
  *
  */
-class SelectedPartsFragment(val partType: ComputerPart.ComputerPartType): Fragment() {
+class SelectedPartsFragment(private val partType: ComputerPart.ComputerPartType): Fragment() {
+
+    lateinit var binding : FragmentPartOverviewBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding : FragmentPartOverviewBinding = DataBindingUtil.inflate(
+        val model = ViewModelProviders.of(this)[AssemblyViewModel::class.java]
+
+        binding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_part_overview, container, false)
 
         binding.partType = partType
         binding.containerEmpty = true
+        binding.viewModel = model
 
-        // Inflate the layout for this fragment
+        subscribeOnActions(model)
+
         return binding.root
+    }
+
+    private fun subscribeOnActions(model : AssemblyViewModel) {
+        model.nextAction.observe(this, Observer<AssemblyViewModel.AssemblyAction> {
+            when (it) {
+                getAssemblyActionFromType() -> {
+                    addPart()
+                    model.actionDone()
+                }
+                else -> {}
+            }
+        })
+    }
+
+    private fun getAssemblyActionFromType() : AssemblyViewModel.AssemblyAction = when (partType) {
+        ComputerPart.ComputerPartType.CPU -> AssemblyViewModel.AssemblyAction.ADD_CPU
+        ComputerPart.ComputerPartType.MEMORY_MODULE -> AssemblyViewModel.AssemblyAction.ADD_MEMORY
+        ComputerPart.ComputerPartType.MOTHERBOARD -> AssemblyViewModel.AssemblyAction.ADD_MOTHERBOARD
+        ComputerPart.ComputerPartType.ASSEMBLED_PC -> throw IllegalStateException()
+    }
+
+    private fun addPart() {
+        //TODO implement browser pick action
+        //BrowserActivity.launchForPickPartFrom(activity, partType)
+
+        val model = ViewModelProviders.of(this)[AssemblyViewModel::class.java]
+        model.newPartAdded(ModelUtils.generateRandomPartOfType(partType), partType)
+
+        binding.containerEmpty = false
     }
 
     companion object {
